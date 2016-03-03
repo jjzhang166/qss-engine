9a4e1766060dad3e78a881afb91a7d78f60f28b4
Joaquin Fernandez <joaquin.f.fernandez@gmail.com>
2016-03-03 17:24:12 -0300

    Modified Mac build script.

--- deploy/mac/build.sh
+++ deploy/mac/build.sh
@@ -20,19 +20,34 @@
 #===================================================================================
 
 cd ../../
-echo "Retrieving latest from SVN";
-svn update > rev
-tail rev | awk '/At/ {print $NF}' | head -c 4 > rvn
+echo "Retrieving latest from Git";
+git pull
+head ./doc/version.major -c 4 > vm
+git rev-list --count HEAD > rvn
+cat vm rvn > version
 REV=`cat rvn`
-./deploy/linux/setRevision.sh ./deploy/mac/qss-solver.ini $REV 
+./deploy/linux/setRevision.sh ./deploy/mac/qss-solver.ini $REV
+VER=`cat version`
 echo "Done"
-echo "Building QSS Solver MAC bundle.";
+echo "Building QSS Solver MAC bundle version $VER";
 echo "Building Binaries";
 rm -rf ./bin/qss-solver.app
+mkdir -p ./bin/qss-solver.app
+mkdir -p ./bin/qss-solver.app/Contents
+mkdir -p ./bin/qss-solver.app/Contents/MacOS
+mkdir -p ./bin/qss-solver.app/Contents/Resources
+mkdir ./bin/qss-solver.app/Contents/Resources/output
+mkdir ./bin/qss-solver.app/Contents/Resources/build
+mkdir ./bin/qss-solver.app/Contents/Resources/usr
+mkdir ./bin/qss-solver.app/Contents/Resources/usr/libs
+
+rm -rf tmp
+mkdir tmp
+git checkout-index --prefix=tmp/ -a
 cd ./src
 make OS=mac  
 cd ..
-svn export deploy/mac/scripts ./bin/qss-solver.app/Contents/MacOS/scripts
+cp -r ./tmp/deploy/mac/scripts/ ./bin/qss-solver.app/Contents/MacOS
 chmod +x ./bin/qss-solver.app/Contents/MacOS/scripts/*
 cp bin/mmoc  ./bin/qss-solver.app/Contents/MacOS/ 
 cp bin/translate-sbml  ./bin/qss-solver.app/Contents/MacOS/ 
@@ -40,13 +55,11 @@ cp deploy/mac/qss-solver.ini ./bin/qss-solver.app/Contents/MacOS/qss-solver.ini
 cp doc/COPYING  ./bin/qss-solver.app/Contents/MacOS/ 
 cp doc/INSTALL  ./bin/qss-solver.app/Contents/MacOS/ 
 cp doc/README.txt  ./bin/qss-solver.app/Contents/MacOS/ 
-svn export build ./bin/qss-solver.app/Contents/Resources/build
-svn export doc ./bin/qss-solver.app/Contents/Resources/doc
-svn export models ./bin/qss-solver.app/Contents/Resources/models
-svn export output ./bin/qss-solver.app/Contents/Resources/output
-svn export usr ./bin/qss-solver.app/Contents/Resources/usr
-svn export packages ./bin/qss-solver.app/Contents/Resources/packages
-svn export src ./bin/qss-solver.app/Contents/Resources/src
+cp -r ./tmp/doc ./bin/qss-solver.app/Contents/Resources/
+cp -r ./tmp/models ./bin/qss-solver.app/Contents/Resources/
+cp -r ./tmp/usr/* ./bin/qss-solver.app/Contents/Resources/usr/
+cp -r ./tmp/packages ./bin/qss-solver.app/Contents/Resources/
+cp -r ./tmp/src ./bin/qss-solver.app/Contents/Resources/
 cp src/libs/*.a ./bin/qss-solver.app/Contents/Resources/src/libs
 cp usr/libs/*.a ./bin/qss-solver.app/Contents/Resources/usr/libs
 cp /usr/local/lib/libsbml.5.10.2.dylib ./bin/qss-solver.app/Contents/Resources/usr/libs/libsbml.5.dylib
@@ -57,4 +70,4 @@ rm -rf qss-solver.app/Contents/Resources/qt.conf
 macdeployqt qss-solver.app -verbose=2 -dmg
 cd ..
 mv bin/qss-solver.dmg deploy/mac/
-rm ./src/rvn ./src/rev
+rm rvn vm version
