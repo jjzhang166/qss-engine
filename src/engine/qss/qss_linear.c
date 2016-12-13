cfdce7f9ce6ff357acec2777f827d2c914aad4df
Joaquin Fernandez <joaquin.f.fernandez@gmail.com>
2016-12-13 14:23:12 -0300

    fix: Modified mapping data structures for linear search algorithms.
    2016-12-13  Joaquin  <joaquin.f.fernandez@gmail.com>
    
    	* engine/qss/qss_linear.c: Modified mapping data structures for linear
    search 	algorithms.
    	* engine/qss/qss_integrator.c
    	(QSS_PAR_synchronize): Restore originial index after a simulataneous
    step.
    	* engine/qss/qss_data.c Set default shated state flag to false..

--- src/engine/qss/qss_linear.c
+++ src/engine/qss/qss_linear.c
@@ -44,7 +44,7 @@ LN_init (SC_scheduler scheduler, QSS_data simData, QSS_time simTime)
   for (j = 0; j < states; j++)
     {
 #ifdef QSS_PARALLEL
-      if (lp->qInMap[j] > NOT_ASSIGNED)
+      if (lp->qMap[j] > NOT_ASSIGNED)
 	{
 #endif
       if (simTime->nextStateTime[j] < minVar)
@@ -60,7 +60,7 @@ LN_init (SC_scheduler scheduler, QSS_data simData, QSS_time simTime)
   for (j = 0; j < events; j++)
     {
 #ifdef QSS_PARALLEL
-      if (lp->eInMap[j] > NOT_ASSIGNED)
+      if (lp->eMap[j] > NOT_ASSIGNED)
 	{
 #endif
       if (simTime->nextEventTime[j] < minEvent)
@@ -134,7 +134,7 @@ LN_update (SC_scheduler scheduler, QSS_data simData, QSS_time simTime)
   for (j = 0; j < states; j++)
     {
 #ifdef QSS_PARALLEL
-      if (lp->qInMap[j] > NOT_ASSIGNED)
+      if (lp->qMap[j] > NOT_ASSIGNED)
 	{
 #endif
       if (simTime->nextStateTime[j] < minVar)
@@ -154,7 +154,7 @@ LN_update (SC_scheduler scheduler, QSS_data simData, QSS_time simTime)
 	  for (j = 0; j < events; j++)
 	    {
 #ifdef QSS_PARALLEL
-	      if (lp->eInMap[j] > NOT_ASSIGNED)
+	      if (lp->eMap[j] > NOT_ASSIGNED)
 		{
 #endif
 	      if (simTime->nextEventTime[j] < minEvent)
@@ -175,7 +175,7 @@ LN_update (SC_scheduler scheduler, QSS_data simData, QSS_time simTime)
       for (j = 0; j < events; j++)
 	{
 #ifdef QSS_PARALLEL
-	  if (lp->eInMap[j] > NOT_ASSIGNED)
+	  if (lp->eMap[j] > NOT_ASSIGNED)
 	    {
 #endif
 	  if (simTime->nextEventTime[j] < minEvent)
