a487678ae5c3f6933e38f212e4a7eee60bf3b38b
Federico Bergero <fedebergero@gmail.com>
2017-04-03 11:38:57 -0300

    Step output working

--- src/engine/classic/classic_cvode_integrator.c
+++ src/engine/classic/classic_cvode_integrator.c
@@ -25,6 +25,7 @@
 #include <sundials/sundials_dense.h> /* definitions DlsMat DENSE_ELEM */
 #include <sundials/sundials_types.h> /* definition of type realtype */
 
+<<<<<<< 471112e85ae614a0950f06a53201939be85cb969
 //#define USE_JACOBIAN
 #ifdef USE_JACOBIAN
 #include <cvode/cvode_superlumt.h>   /* prototype for CVSUPERLUMT */
@@ -32,6 +33,8 @@
 #endif
 
 
+=======
+>>>>>>> Step output working
 #include <classic/classic_cvode_integrator.h>
 
 #include <stdio.h>
@@ -55,6 +58,7 @@ static SD_output simOutput = NULL;
 
 int is_sampled;
 
+<<<<<<< 471112e85ae614a0950f06a53201939be85cb969
 #ifdef USE_JACOBIAN
 /* Test jacobian */
 #define N 200
@@ -99,12 +103,40 @@ static int Jac(realtype t, N_Vector y, N_Vector fy, SlsMat JacMat, void *user_da
 
 /* Test jacobian */
 static int check_flag(void *flagvalue, const char *funcname, int opt, CLC_simulator simulator)
+=======
+/*
+int dopri_percentage = 0;
+
+struct simData_DOPRI
+{
+  double **solution;
+  double *solution_time;
+  double **outvar;
+  double *x;
+  double *t;
+  unsigned long *totalOutputSteps;
+  double last_step;
+  double step_size;
+  double *temp_x;
+  double final_time;
+  int size;
+};
+
+struct simData_DOPRI simDataDopri;
+
+*/
+static int check_flag(void *flagvalue, const char *funcname, int opt)
+>>>>>>> Step output working
 {
   int *errflag;
 
   /* Check if SUNDIALS function returned NULL pointer - no memory allocated */
   if (opt == 0 && flagvalue == NULL) {
+<<<<<<< 471112e85ae614a0950f06a53201939be85cb969
      SD_print (simulator->simulationLog, "\nSUNDIALS_ERROR: %s() failed - returned NULL pointer\n\n", funcname);
+=======
+    fprintf(stderr, "\nSUNDIALS_ERROR: %s() failed - returned NULL pointer\n\n", funcname);
+>>>>>>> Step output working
     return(1); 
   }
 
@@ -112,27 +144,42 @@ static int check_flag(void *flagvalue, const char *funcname, int opt, CLC_simula
   else if (opt == 1) {
     errflag = (int *) flagvalue;
     if (*errflag < 0) {
+<<<<<<< 471112e85ae614a0950f06a53201939be85cb969
       SD_print (simulator->simulationLog, "\nSUNDIALS_ERROR: %s() failed with flag = %d\n\n", funcname, *errflag);
+=======
+      fprintf(stderr, "\nSUNDIALS_ERROR: %s() failed with flag = %d\n\n", funcname, *errflag);
+>>>>>>> Step output working
       return(1); 
     }
   }
 
   /* Check if function returned NULL pointer - no memory allocated */
   else if (opt == 2 && flagvalue == NULL) {
+<<<<<<< 471112e85ae614a0950f06a53201939be85cb969
     SD_print (simulator->simulationLog, "\nMEMORY_ERROR: %s() failed - returned NULL pointer\n\n", funcname);
+=======
+    fprintf(stderr, "\nMEMORY_ERROR: %s() failed - returned NULL pointer\n\n", funcname);
+>>>>>>> Step output working
     return(1); 
   }
 
   return(0);
 }
 
+<<<<<<< 471112e85ae614a0950f06a53201939be85cb969
 int CVODE_model (realtype t, N_Vector y, N_Vector ydot, void *user_data) {
+=======
+int 
+CVODE_model (realtype t, N_Vector y, N_Vector ydot, void *user_data)
+{
+>>>>>>> Step output working
   clcData->funEvaluations++;
   clcModel->f (NV_DATA_S(y), clcData->d, clcData->alg, t, NV_DATA_S(ydot));
   return 0;
 }
 
 
+<<<<<<< 471112e85ae614a0950f06a53201939be85cb969
 int CVODE_events (realtype t, N_Vector y, realtype *gout, void *user_data) {
   double out;
   int i;
@@ -143,6 +190,59 @@ int CVODE_events (realtype t, N_Vector y, realtype *gout, void *user_data) {
   return 0;
 }
 
+=======
+/*
+void
+DOPRI_events (int n, double x, const double *y, double *g)
+{
+  double out;
+  clcModel->events->zeroCrossing (n, y, clcData->d, clcData->alg, x, &out);
+  g[0] = out + clcData->event[n].zcSign * HIST;
+}
+
+void
+DOPRI_solout (long nr, double xold, double x, double* y, unsigned n, int* irtrn,
+	      int *jroot)
+{
+  const double _ft = clcData->ft;
+#ifdef SYNC_RT
+  waitUntil(x);
+#endif
+  if (irtrn[0] == 3) 
+    CLC_handle_event (clcData, clcModel, y, jroot, x, NULL);
+  if (!is_sampled) {
+      clcData->totalSteps++;
+      CLC_save_step (simOutput, simDataDopri.solution,
+		     simDataDopri.solution_time, x,
+		     simDataDopri.totalOutputSteps[0], y, clcData->d,
+		     clcData->alg);
+      simDataDopri.totalOutputSteps[0]++;
+  } else { // Do sample
+      while (simDataDopri.last_step+simDataDopri.step_size<x) {
+        // Skip last step
+        if (fabs(simDataDopri.last_step+simDataDopri.step_size-simDataDopri.final_time)/simDataDopri.step_size < 1)
+          break;
+        clcData->totalSteps++;
+        int i;
+        for (i=0;i< simDataDopri.size; i++)
+          simDataDopri.temp_x[i] = contd5(i,simDataDopri.last_step+simDataDopri.step_size);
+        CLC_save_step (simOutput, simDataDopri.solution,
+		       simDataDopri.solution_time, simDataDopri.last_step+simDataDopri.step_size,
+		       simDataDopri.totalOutputSteps[0], simDataDopri.temp_x, clcData->d,
+		       clcData->alg);
+        simDataDopri.totalOutputSteps[0]++;
+        simDataDopri.last_step+=simDataDopri.step_size;
+      }
+  }
+  if ((int) (x * 100 / _ft) > dopri_percentage) {
+    dopri_percentage = 100 * x / _ft;
+    fprintf (stderr, "*%g", x);
+    fflush (stderr);
+  }
+}
+
+*/
+>>>>>>> Step output working
 void
 CVODE_integrate (SIM_simulator simulate)
 {
@@ -168,6 +268,7 @@ CVODE_integrate (SIM_simulator simulate)
   double *solution_time = malloc (sizeof(double) * num_steps);
   double **outvar = malloc (sizeof(double) * simOutput->outputs);
   int *jroot = malloc (sizeof(int) * clcData->events), flag;
+<<<<<<< 471112e85ae614a0950f06a53201939be85cb969
   int size = clcData->states, nnz;
   int event_detected = 0;
   double rel_tol = dQRel, abs_tol = dQMin;
@@ -186,11 +287,34 @@ CVODE_integrate (SIM_simulator simulate)
   if (check_flag((void *)abstol, "N_VNew_Serial", 0, simulator)) return;
 
   for (i = 0; i < clcData->states; i++) {
+=======
+  double troot = 0;
+  int size = clcData->states, res;
+  double /**x, *temp_x, */rel_tol = dQRel, abs_tol = dQMin;
+  realtype reltol = rel_tol, t = clcData->it, tout;
+  y = abstol = NULL;
+  int percentage = 0;
+  ///x = checkedMalloc (sizeof(double) * clcData->states);
+  //temp_x = checkedMalloc (sizeof(double) * clcData->states);
+  CLC_compute_outputs (simOutput, solution, num_steps);
+
+  cvode_mem = CVodeCreate(CV_BDF, CV_NEWTON);
+
+  y = N_VNew_Serial(size);
+  if (check_flag((void *)y, "N_VNew_Serial", 0)) return;
+
+  abstol = N_VNew_Serial(size);
+  if (check_flag((void *)abstol, "N_VNew_Serial", 0)) return;
+
+  for (i = 0; i < clcData->states; i++) {
+    //x[i] = _x[i];
+>>>>>>> Step output working
     Ith(y,i) = _x[i];
     Ith(abstol,i) = abs_tol;
   }
   
   flag = CVodeInit(cvode_mem, CVODE_model, t, y);
+<<<<<<< 471112e85ae614a0950f06a53201939be85cb969
   if (check_flag((void *)cvode_mem, "CVodeCreate", 0, simulator)) return;
 
   flag = CVodeSVtolerances(cvode_mem, reltol, abstol);
@@ -215,6 +339,28 @@ CVODE_integrate (SIM_simulator simulate)
 #endif
 
 /***************************************************************/
+=======
+  if (check_flag((void *)cvode_mem, "CVodeCreate", 0)) return;
+
+  flag = CVodeSVtolerances(cvode_mem, reltol, abstol);
+  if (check_flag(&flag, "CVodeInit", 1)) return;
+
+  flag = CVDense(cvode_mem, size);
+  if (check_flag(&flag, "CVDense", 1)) return;
+
+/*
+  simDataDopri.solution = solution;
+  simDataDopri.solution_time = solution_time;
+  simDataDopri.x = x;
+  simDataDopri.temp_x = temp_x;
+  simDataDopri.outvar = outvar;
+  simDataDopri.totalOutputSteps = &totalOutputSteps;
+  simDataDopri.step_size = step_size;
+  simDataDopri.final_time = _ft ;
+  simDataDopri.size= clcData->states;
+*/
+  //dopri_percentage = 0;
+>>>>>>> Step output working
   getTime (simulator->stats->sTime);
   if (is_sampled) {
     CLC_save_step (simOutput, solution, solution_time, t, totalOutputSteps, NV_DATA_S(y), clcData->d, clcData->alg);
@@ -227,6 +373,7 @@ CVODE_integrate (SIM_simulator simulate)
   while (t < _ft) {
     if (!is_sampled) 
 	    tout = _ft;
+<<<<<<< 471112e85ae614a0950f06a53201939be85cb969
     else {
 	    if (!event_detected)
 	      tout = t + step_size;
@@ -279,6 +426,15 @@ CVODE_integrate (SIM_simulator simulate)
     		  CLC_save_step (simOutput, solution, solution_time, t, totalOutputSteps, NV_DATA_S(y), clcData->d, clcData->alg);
 		      totalOutputSteps++;
       }
+=======
+    if (tout > _ft)
+	    tout = _ft;
+    flag = CVode(cvode_mem, tout, y, &t, CV_ONE_STEP);
+    if (flag == CV_SUCCESS) {
+      printf("%g %g %g\n", t , NV_DATA_S(y)[0], NV_DATA_S(y)[1]);
+	    CLC_save_step (simOutput, solution, solution_time, t, clcData->totalOutputSteps,NV_DATA_S(y), clcData->d, clcData->alg);
+	    clcData->totalOutputSteps++;
+>>>>>>> Step output working
     } else { 
       SD_print (simulator->simulationLog, "CVODE failed at t=%g\n",t);
       break;
@@ -292,7 +448,26 @@ CVODE_integrate (SIM_simulator simulate)
   waitUntil(t);
 #endif
   }
+<<<<<<< 471112e85ae614a0950f06a53201939be85cb969
   getTime (simulator->stats->sTime);
+=======
+/*
+  res = dopri5 (size, DOPRI_model, t, x, _ft , &rel_tol, &abs_tol, 0,
+		    DOPRI_solout, 2,
+		    stdout,
+		    1e-30, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 2000000000, 0, -1,
+		    size,
+		    NULL,
+		    0, DOPRI_events, clcData->events, jroot, &troot);
+  if (res < 0)
+	{
+	  SD_print (simulator->simulationLog,
+		    "DOPRI failed with res=%d at %g\n", res, t);
+	  printf ("DOPRI failed with res=%d at %g\n", res, t);
+	}
+*/
+
+>>>>>>> Step output working
   if (is_sampled)
 	{
 	  if (totalOutputSteps < num_steps)
@@ -301,6 +476,7 @@ CVODE_integrate (SIM_simulator simulate)
         totalOutputSteps++;
 	    }
 	}
+<<<<<<< 471112e85ae614a0950f06a53201939be85cb969
   subTime (simulator->stats->sTime, simulator->stats->iTime);
   if (simulator->settings->debug == 0 || simulator->settings->debug > 1)
     {
@@ -317,16 +493,33 @@ CVODE_integrate (SIM_simulator simulate)
       nni += val;
 
       SD_print (simulator->simulationLog, "Simulation time (CVODE %s):", clcData->solver == SD_CVODE_BDF ? "BDF" : "ADAMS");
+=======
+  getTime (simulator->stats->sTime);
+  subTime (simulator->stats->sTime, simulator->stats->iTime);
+  if (simulator->settings->debug == 0 || simulator->settings->debug > 1)
+    {
+      SD_print (simulator->simulationLog, "Simulation time (CVODE):");
+>>>>>>> Step output working
       SD_print (simulator->simulationLog, "----------------");
       SD_print (simulator->simulationLog, "Miliseconds: %g", getTimeValue (simulator->stats->sTime));
       SD_print (simulator->simulationLog, "Scalar function evaluations: %d", clcData->scalarEvaluations);
       SD_print (simulator->simulationLog, "Individual Zero Crossings : %d", clcData->zeroCrossings);
       SD_print (simulator->simulationLog, "Function evaluations: %llu", clcData->funEvaluations);
+<<<<<<< 471112e85ae614a0950f06a53201939be85cb969
       SD_print (simulator->simulationLog, "Function evaluations (reported by CVODE): %ld",nfe);
       SD_print (simulator->simulationLog, "Output steps: %d", totalOutputSteps);
       SD_print (simulator->simulationLog, "Simulation steps: %ld", nst);
       SD_print (simulator->simulationLog, "Simulation steps (rejected) : %ld", netf);
       SD_print (simulator->simulationLog, "Newton iterations performed: (reported by CVODE): %ld", nni);
+=======
+      //printLog("Function evaluations (reported by DASSL): %d",totalStepsDASSL);
+      //printLog("Zero crossing evaluations : %d", totalCrossingEvaluations );
+      SD_print (simulator->simulationLog, "Output steps: %d", totalOutputSteps);
+      //SD_print (simulator->simulationLog, "Simulation steps: %d", nstepRead ());
+      //SD_print (simulator->simulationLog, "Simulation steps (accepted) : %d", naccptRead ());
+      //SD_print (simulator->simulationLog, "Simulation steps (rejected) : %d", nrejctRead ());
+      //SD_print (simulator->simulationLog, "Function evaluations (reported by DOPRI): %d", nfcnRead ());
+>>>>>>> Step output working
       SD_print (simulator->simulationLog, "Events detected : %d", clcData->totalEvents);
     }
   CLC_write_output (simOutput, solution, solution_time, totalOutputSteps);
